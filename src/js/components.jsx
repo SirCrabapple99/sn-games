9d25ad34b506c17645c3e416d35a57ce151c2af2
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2026-09-10 20:46:05 -0500

    more css changes

--- src/js/components.jsx
+++ src/js/components.jsx
@@ -3,7 +3,7 @@ export const Game = function (cx) {
   return (
     <div class="game-tilt" onclick={this.click} onpointerup={this.pointerup} onpointercancel={this.pointercancel} onpointerdown={this.pointerdown}>
       <div class="game-card">
-        <img class="game-image" src={this.cover} alt={this.title}></img>
+        <img class="game-image" src={this.cover} alt="game image" draggable="false" />
         <div class="game-text ui">{this.title}</div>
         {cx.children}
       </div>
