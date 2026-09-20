549dbd86422d6be29e86a9253d91ba65de312bc8
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2026-09-19 23:57:02 -0500

    mobile?

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
 // add vendor prefixes because I'm lazy
-export default {
-  plugins: {
-    autoprefixer: {},
+import autoprefixer from "autoprefixer";
+
+const noImageSave = () => ({
+  postcssPlugin: "no-image-save",
+  Once(root, { Rule, Declaration }) {
+    const rule = new Rule({ selector: "img" });
+    rule.append(new Declaration({ prop: "-webkit-touch-callout", value: "none" }));
+    rule.append(new Declaration({ prop: "-webkit-user-select", value: "none" }));
+    rule.append(new Declaration({ prop: "user-select", value: "none" }));
+    root.append(rule);
   },
+});
+noImageSave.postcss = true;
+
+export default {
+  plugins: [noImageSave(), autoprefixer()],
 };
