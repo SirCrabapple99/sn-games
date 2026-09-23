f81da540fece37946e3af4c79151460c36f710d9
SirCrabapple99 <@>
2026-09-23 13:16:20 -0500

    h

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { jsxPlugin } from 'dreamland/vite';
+/* import { viteSingleFile } from 'vite-plugin-singlefile'; */
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/sn-games/',
-  plugins: [jsxPlugin()],
+  plugins: [jsxPlugin()/* , viteSingleFile() */],
 });
